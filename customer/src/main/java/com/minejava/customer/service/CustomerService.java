56ab57cd61692951e762c4f48db9ee0579aede2c
Nath <nathaniel@aims.ac.za>
2022-01-22 17:19:07 +0000

    Added jpa, hibernate etc configs

--- customer/src/main/java/com/minejava/customer/service/CustomerService.java
+++ customer/src/main/java/com/minejava/customer/service/CustomerService.java
@@ -1,12 +1,13 @@
 package com.minejava.customer.service;
 
 import com.minejava.customer.model.Customer;
+import com.minejava.customer.repository.CustomerRepository;
 import com.minejava.customer.request.CustomerRegistrationRequest;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest  request) {
 
         Customer customer = Customer.builder()
@@ -17,5 +18,6 @@ public record CustomerService() {
         // todo: check if email is valid
     //todo: check if email is not taken
     // todo: check if customer is in db
+    customerRepository.save(customer);
     }
 }
